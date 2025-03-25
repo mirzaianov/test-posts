ee75b7ccda5e40de4455b3828d6c9c6f7db88767
Riaz Mirzaianov <7o.mseJak@gmail.com>
2025-03-25 16:33:16 +0300

    feat: Add post design

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import Posts from './components/posts';
 
 export default function App() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between py-10">
-      <h1>Posts</h1>
-      <Posts />
+    <main className="flex min-h-screen flex-col items-center justify-start py-10">
+      <div className="flex w-10/12 max-w-3xl flex-col items-center gap-10">
+        <h1 className="text-3xl font-bold underline underline-offset-3">
+          Posts
+        </h1>
+        <Posts />
+      </div>
     </main>
   );
 }
--- src/components/posts.tsx
+++ src/components/posts.tsx
@@ -1,27 +1,55 @@
 import useFetch from '../hooks/useFetch';
-import { POSTS_URL } from '../constants';
-import { type Post } from '../types';
+import { POSTS_URL, USERS_URL } from '../constants';
+import { type User, type Post } from '../types';
+import { Avatar, AvatarFallback, AvatarImage } from '../components/ui/avatar';
 
 export default function Posts() {
-  const { data, isLoading, error } = useFetch<Post[]>(POSTS_URL);
+  const {
+    data: postsData,
+    isLoading: postsIsLoading,
+    error: postsError,
+  } = useFetch<Post[]>(POSTS_URL);
 
-  if (isLoading) {
+  const {
+    data: usersData,
+    isLoading: usersIsLoading,
+    error: usersError,
+  } = useFetch<User[]>(USERS_URL);
+
+  if (postsIsLoading || usersIsLoading) {
     return <div>Loading...</div>;
   }
 
-  if (error) {
-    return <div>{error.message}</div>;
+  if (postsError || usersError) {
+    return <div>{postsError?.message || usersError?.message}</div>;
   }
 
+  const postWithUserName = postsData?.map((post) => ({
+    ...post,
+    userName: usersData?.find((user) => user.id === post.userId)?.name,
+  }));
+
   return (
-    <ul>
-      {data &&
-        data.map((post) => (
-          <li key={post.id}>
-            <h2>{post.title}</h2>
+    <>
+      {postWithUserName?.map((post) => (
+        <div
+          key={post.id}
+          className="grid gap-x-4 gap-y-2"
+        >
+          <Avatar className="self-center">
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback className="flex gap-0.5">
+              <span>{post.userName?.split(' ')[0][0] || 'A'}</span>
+              <span>{post.userName?.split(' ')[1][0] || 'B'}</span>
+            </AvatarFallback>
+          </Avatar>
+          <p className="self-center">{post.userName}</p>
+          <div className="col-start-2">
+            <h3>{post.title}</h3>
             <p>{post.body}</p>
-          </li>
-        ))}
-    </ul>
+          </div>
+        </div>
+      ))}
+    </>
   );
 }
