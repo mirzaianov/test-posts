a56bc5ade1d978e71a2562f2177e3e71a99b53d8
Riaz Mirzaianov <7o.mseJak@gmail.com>
2025-03-26 15:30:11 +0300

    feat: Update font

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start py-10">
       <div className="flex w-10/12 max-w-3xl flex-col items-center gap-10">
-        <h1 className="text-3xl font-bold underline underline-offset-3">
+        <h1 className="ultra-regular text-5xl font-bold underline underline-offset-4">
           Posts
         </h1>
         <Posts />
